dc431a59fcfc7bfd8f647fb75c4206dcfc5f6073
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-05-02 17:17:02 +0100

    Change the default action to produce a graph.
    The default action creates data for a line graph which shows
    the number of stories over a time line matching any query criteria
    passed.
    eg.
    /?start_date=2014/01/01&end_date=2014/05/02&label=smart-answer&query=passport,visa,marriage,death,birth
    gives statistics on stories labelled smart-answer broken down by
    the phrases listed in the query param.

--- app.rb
+++ app.rb
@@ -5,23 +5,60 @@ helpers do
   def from_env(key)
     ENV[key] || raise("You must set the #{key} environment variable")
   end
-  def story_meta(story)
-    meta = [story.story_type]
-    if story.story_type == 'feature'
-      if story.estimate > 0
-        meta << story.estimate
-      else
-        meta << "unestimated"
+
+  def weekly_data(headings, stories, start_date, end_date, terms = nil)
+    data = [headings]
+    (start_date..end_date).each_slice(7) do |week|
+      key = week.first.strftime("%d/%m/%Y")
+      weekly_stories = [key]
+      stories_this_week = stories.select do |s|
+        s.created_at >= week.first and s.created_at <= week.last
       end
+      weekly_stories << stories_this_week.size
+
+      if terms
+        terms.each do |term|
+          terms_ary = []
+          re = Regexp.new(term, 'i')
+          terms_ary << stories_this_week.select do |s|
+            s.name =~ re or s.description =~ re
+          end.size
+          weekly_stories << terms_ary
+        end
+      end
+      data << weekly_stories.flatten
     end
-    meta << story.current_state
-    meta
+    data
+  end
+
+  def query_terms(query)
+    query.split(",").map(&:strip)
   end
 end
 
 get '/' do
   PivotalTracker::Client.token(from_env('USERNAME'), from_env('PASSWORD'))
   project = PivotalTracker::Project.find(from_env('PROJECT'))
-  stories = project.stories.all(:label => from_env('LABEL'))
-  erb :index, locals: { project: project, stories: stories }
+  story_filter = { :includedone => true }
+  story_filter[:label] = params[:label] if params[:label]
+
+  stories = project.stories.all(story_filter)
+
+  start_date = Date.parse(params[:start_date])
+  end_date = Date.parse(params[:end_date])
+
+  headings = ["Week starting", "Total stories (for label)"]
+  if params[:query]
+    terms = query_terms(params[:query])
+    headings += terms
+  else
+    terms = nil
+  end
+
+  data_array = weekly_data(headings, stories, start_date, end_date, terms)
+
+  erb :index, locals: {
+    project: project,
+    data_array: data_array
+  }
 end
